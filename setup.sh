ad90d46a8e10dea315445464190377795fef1fac
Shyam R Nair <66541589+shyamrn@users.noreply.github.com>
2022-07-23 18:25:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
-email = \"shyamieee@gmail.com\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS = false\n\
+enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
